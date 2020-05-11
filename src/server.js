853ad4563779fc07c3f82409e30b40416dfe9159
Gabriel Tinoco <tinocogabriel@gmail.com>
2020-05-11 19:33:36 -0300

    CRUD finalizado

--- src/server.js
+++ src/server.js
@@ -3,7 +3,23 @@ const app = express();
 
 const routes = require("./routes");
 
+app.use(express.json());
 app.use(routes);
+
+//Not Found
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404; 
+  next(error);
+});
+
+//catch all
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: error.message,
+  });
+});
 
 app.listen(3333, () => {
   console.log("server is running");
