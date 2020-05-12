a893d8158c308b6851faa9a378a4adb8511e75ed
Gabriel Tinoco <tinocogabriel@gmail.com>
2020-05-11 21:07:27 -0300

    Procedures e Triggers

--- knexfile.js
+++ knexfile.js
@@ -1,53 +1,56 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'aula-nodejs-knex',
-      user:     'tinoco',
-      password: '123456789'
+      database: "aula-nodejs-knex",
+      user: "tinoco",
+      password: "123456789",
     },
-    migrations:{
-      tableName: 'knex-migrations',
-      directory: `${__dirname}/src/database/migrations`
+    migrations: {
+      tableName: "knex-migrations",
+      directory: `${__dirname}/src/database/migrations`,
     },
-    seeds:{
-      directory: `${__dirname}/src/database/seeds`
-    }
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
   },
-
+  onUpdateTrigger: (table) => ` 
+  CREATE TRIGGER ${table}_updated_at
+  BEFORE UPDATE ON ${table}
+  FOR EACH ROW
+  EXECUTE PROCEDURE on_update_timestamp();
+  `,
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
---
+++ src/database/migrations/20200311204632_add_custom_procedure.js
@@ -0,0 +1,15 @@
+const CUSTOM_FUNCTION = `
+CREATE OR REPLACE FUNCTION on_update_timestamp()
+RETURNS trigger AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+`;
+
+const DROP_CUSTOM_FUNCTION = `DROP FUNCTION on_update_timestamp()`;
+
+exports.up = async (knex) => knex.raw(CUSTOM_FUNCTION);
+
+exports.down = async (knex) => knex.raw(DROP_CUSTOM_FUNCTION);
